652e1c8cde317df097e1d5bf37a63cb840c4b64d
Emmacharleswilson <emmacharleswilson@googlemail.com>
2021-10-15 17:08:56 +0000

    Fix issue with JavaScript code and remove placeholder text for the question's

--- game.js
+++ game.js
@@ -2,88 +2,86 @@ const question = document.querySelector('#question');
 const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
-const progressBarFull = document.querySelector('#progressBarFull');
 
-let currentQuestion = {}
-let acceptingAnswers = true 
-let score = 0 
-let questionCounter = 0
-let availableQuestions = []
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
 
 let questions = [
     {
-        question: 'What is 2 + 2?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '17',
-        answer: 2,
-    },
-    {
-        question: 'What is 3 + 3?',
-        choice1: '2',
-        choice2: '6',
-        choice3: '21',
-        choice4: '17',
-        answer: 2,
-    },
-    {
-        question: 'What is 4 + 4?',
-        choice1: '2',
-        choice2: '8',
-        choice3: '21',
-        choice4: '17',
-        answer: 2,
-    },
-    {
-        question: 'What is 5 + 5?',
-        choice1: '2',
-        choice2: '10',
-        choice3: '21',
-        choice4: '17',
-        answer: 2,
-    },
-    {
-        question: 'What is 1 + 1?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '17',
+        question: '',
+        choice1: '',
+        choice2: '',
+        choice3: '',
+        choice4: '',
         answer: 1,
-    }
-]
+    },
+    {
+        question: '',
+        choice1: '',
+        choice2: '',
+        choice3: '',
+        choice4: '',
+        answer: ,
+    },
+    {
+        question: '',
+        choice1: '',
+        choice2: '',
+        choice3: '',
+        choice4: '',
+        answer: ,
+    },
+    {
+        question: '',
+        choice1: '',
+        choice2: '',
+        choice3: '',
+        choice4: '',
+        answer: ,
+    },
+    {
+        question: '',
+        choice1: '',
+        choice2: '',
+        choice3: '',
+        choice4: '',
+        answer: ,
+    },
+];
 
 const SCORE_POINTS = 100
-const MAX_QUESTIONS = 5
+const MAX_QUESTIONS = 10
 
-startGame = () => {
+function startgame() {
     questionCounter = 0
     score = 0
     availableQuestions = [...questions]
     getNewQuestion()
-}
+};
 
-getNewQuestion = () => {
-    if(availableQuestions.length === 0 || questionsCounter > MAX_QUESTIONS) {
-        localStorage.setItem('mostRecentScore', score)
+function getNewQuestion() {
+    if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score);
 
-        return window.location.assign('/end.html')
+        return window.location.assign('/end.html');
     }
 
     questionCounter++
-    progressText.innerTEXT = `Question ${questionCounter} of ${MAX_QUESTIONS}`
-    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
 
-    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
-    currentQuestion = availableQuestions[questionsIndex]
-    question.innerTEXT = currentQuestion.question
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionIndex]
+    question.innerText = currentQuestion.question
 
     choices.forEach(choice => {
         const number = choice.dataset['number']
-        choice.innerTEXT = currentQuestion['choice' + number]
+        choice.innerText = currentQuestion['choice' + number]
     })
 
-    availableQuestions.splice(questionsIndex, 1)
+    availableQuestions.splice(questionIndex, 1)
 
     acceptingAnswers = true
 }
@@ -107,7 +105,13 @@ choices.forEach(choice => {
         setTimeout(() => {
             selectedChoice.parentElement.classList.remove(classToApply)
             getNewQuestion()
-        
         }, 1000)
     })
 })
+
+incrementScore = num => {
+    score +=num
+    scoreText.innerText = score
+}
+
+startgame()

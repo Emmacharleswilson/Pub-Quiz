85d14b08081db08f6abd4f4b7bf32f2afc8efdef
Emmacharleswilson <emmacharleswilson@googlemail.com>
2021-10-21 11:58:07 +0000

    Move Javascript for gif animation into seperate file (gif.js)

---
+++ assets/js/gif.js
@@ -0,0 +1,17 @@
+/** fade in effect on gif */
+
+window.onload = fadeIn;
+
+function fadeIn() {
+    var fade = document.getElementById("gif");
+    var opacity = 0;
+    var intervalID = setInterval(function () {
+
+        if (opacity < 1) {
+            opacity = opacity + 0.1
+            fade.style.opacity = opacity;
+        } else {
+            clearInterval(intervalID);
+        }
+    }, 100);
+}
--- assets/js/title.js
+++ assets/js/title.js
@@ -1,28 +1,12 @@
-/** fade in effect on gif */
-
-window.onload = fadeIn;
-
-        function fadeIn() {
-            var fade = document.getElementById("gif");
-            var opacity = 0;
-            var intervalID = setInterval(function() {
-  
-                if (opacity < 1) {
-                    opacity = opacity + 0.1
-                    fade.style.opacity = opacity;
-                } else {
-                    clearInterval(intervalID);
-                }
-            }, 100);
-        }
-
 /** title animation */
 
 // Wrap every letter in a span
 var textWrapper = document.querySelector('.ml6 .letters');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-anime.timeline({loop: true})
+anime.timeline({
+    loop: true
+  })
   .add({
     targets: '.ml6 .letter',
     translateY: ["1.1em", 0],

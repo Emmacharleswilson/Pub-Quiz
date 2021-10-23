4684f32a73e9769572851c54b0b6b8c5b9d4abbe
Emmacharleswilson <emmacharleswilson@googlemail.com>
2021-10-23 11:12:06 +0000

    Add missing semicolon's in gif.js and highscores.js

--- assets/js/gif.js
+++ assets/js/gif.js
@@ -8,7 +8,7 @@ function fadeIn() {
     var intervalID = setInterval(function () {
 
         if (opacity < 1) {
-            opacity = opacity + 0.1
+            opacity = opacity + 0.1;
             fade.style.opacity = opacity;
         } else {
             clearInterval(intervalID);

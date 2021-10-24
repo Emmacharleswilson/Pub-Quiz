bb7b02766bcb0f4a17f58241533bb3e922882453
Emmacharleswilson <emmacharleswilson@googlemail.com>
2021-10-24 13:12:05 +0000

    Add comments to highscores.js

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,8 +1,10 @@
 /*jshint esversion: 6 */
 // Highscores variables 
+
 const highScoresList = document.querySelector('#highScoresList');
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
+// function to return user name and user score to create a list
 highScoresList.innerHTML =
     highScores.map(score => {
         return `<li class="high-score">${score.name} - ${score.score}</li>`;
